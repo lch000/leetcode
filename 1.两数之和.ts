21deb0499e395ee3225d1d5cb812196fe41784ce
liuchuanhe <liuchuanhe@baichuan-inc.com>
2025-04-14 17:41:46 +0800

    feat: 初始化测试

---
+++ 1.两数之和.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
+// @lc code=end
